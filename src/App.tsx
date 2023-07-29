b60e1289d8fa9858bb0c3d3eeea7ce3bff267af8
Emma Sargeant <emma.sargeant@sojo.uk>
2023-07-29 17:02:24 +0100

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex h-[90vh] justify-center items-center w-11/12'>
+      <div className='max-w[400px] text-center'>
+    <h1 className="text-3xl font-medium tracking-wide  text-gray-500">
+      Emma Sargeant
+    </h1>
+     <h2 className="text-xl font-light tracking-wide text-gray-500">
+     Portfolio
+   </h2></div></div>
+  );  
+}
+
+export default App;
